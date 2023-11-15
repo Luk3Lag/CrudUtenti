d1626f9a297d9b5906ffda29d07557c2fbe692f5
Lagana' Luca <150060898+Luk3Lag@users.noreply.github.com>
2023-11-15 12:23:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main{
@@ -11,6 +12,12 @@ public class Main{
         switch (choice) {
             case 1:
                 utente = login.registrazione();
+                if(utente==null){
+                    System.out.println("Login");
+                    utente = login.login();
+                }else{
+                    System.out.println("Registrazione eseguita con successo!");
+                }
                 break;
             case 2:
                 utente = login.login();
@@ -22,35 +29,44 @@ public class Main{
 
         Crud crud = Crud.getInstance(utente);
 
-        utente.changePassword();
-
-        System.out.println("Utente singolo");
-        crud.readCurrentUtente();
-        System.out.println("\nLista utenti");
-        crud.readUtenti();
-
-        crud.updateUtente(0, "pippo", "pluto");
-
-        System.out.println("Utente singolo");
-        crud.readCurrentUtente();
-        System.out.println("\nLista utenti");
-        crud.readUtenti();
-
-        crud.deleteUtente(utente);
-
-        System.out.println("Utente singolo");
-        crud.readCurrentUtente();
-        System.out.println("\nLista utenti");
-        crud.readUtenti();
-        
-        System.out.println("Logout? (s/n)");
-        scan.nextLine();
-        char scelta = scan.nextLine().charAt(0);
-        if(scelta=='s'){
-            if(login.logout()){
-                System.out.println("Logout eseguito correttamente!");
-                return;
+        do{
+            System.out.println("1. Aggiungi utente corrente\n2. Leggi utenti\n3. Modifica password\n4. Elimina utente\n5. Logout");
+            choice = scan.nextInt();
+            switch(choice){
+                case 1:
+                    crud.createUtente(utente);
+                    break;
+                case 2:
+                    crud.readUtenti();
+                    break;
+                case 3:
+                    System.out.print("Inserisci la password: ");
+                    scan.nextLine();
+                    String password = scan.nextLine();
+                    if(password.equals(utente.getPassword())){
+                        System.out.print("Inserisci la nuova password: ");
+                        String newPassword = scan.nextLine();
+                        Map<Integer, Utente> utenti = crud.getUtenti();
+                        for(Utente u : utenti.values()){
+                            if(u.getUsername().equals(utente.getUsername())){
+                                u.setPassword(newPassword);
+                                System.out.println("Password cambiata con successo!");
+                            }
+                        }
+                    }else{
+                        System.out.println("Password errata!");
+                    }
+                    break;
+                case 4:
+                    crud.deleteUtente(utente);
+                    break;
+                case 5:
+                    if(login.logout()){
+                        System.out.println("Logout eseguito correttamente!");
+                        return;
+                    }
+                    break;
             }
-        }
+        }while(true);
     }
 }
