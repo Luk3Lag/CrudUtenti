901e299e3f87ad45e46609cee611e6286f004c8e
Lagana' Luca <150060898+Luk3Lag@users.noreply.github.com>
2023-11-15 09:34:26 +0100

    Update and rename TestMain.java to Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestMain{
+public class Main{
     public static void main(String[] args) {
         Login login = new Login();
         Utente utente = null;
